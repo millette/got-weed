823ebaa27140b4b5e215d0eb193c75a4d64cd99a
Robin Millette <robin@millette.info>
2018-10-26 05:57:08 -0400

    Support --in-stock flag.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { JSDOM } = require('jsdom')
 const contextRe = / data-context="(\{.+\})"/
 const telRe = /^.*\D\d\D\s\d\D\d/
 
+/*
 const stock = (skus) => got('https://www.sqdc.ca/api/inventory/findInventoryItems', {
   json: true,
   headers: {
@@ -16,59 +17,53 @@ const stock = (skus) => got('https://www.sqdc.ca/api/inventory/findInventoryItem
   body: { skus }
 }).then(({ body }) => body)
 
+const available = (pages) => pages.reduce((a, { stocks, json }) => {
+  const them = json.ProductSearchResults.SearchResults.filter((x) => stocks.indexOf(x.Sku) !== -1)
+  return [...a, ...them]
+}, [])
+*/
+
 const searchString = {
   fr: 'Rechercher',
   en: 'Search'
 }
 
-// const getPage = async (lang, p = 1) => {
 const getPage = async (cli, p = 1) => {
   const lang = cli.flags.language
-
-  // const { headers, body } = await got(`https://www.sqdc.ca/${lang}-CA/${searchString[lang]}?keywords=*&sortDirection=asc&page=${p}`)
   const { body } = await got(`https://www.sqdc.ca/${lang}-CA/${searchString[lang]}?keywords=*&sortDirection=asc&page=${p}`)
   const m1 = body.match(contextRe)
   if (!m1 || !m1[1]) {
     throw new Error('Nothing here')
   }
   return JSON.parse(m1[1].replace(/&quot;/g, '"'))
-  /*
-  const json = JSON.parse(m1[1].replace(/&quot;/g, '"'))
-  const stocks = await stock(json.ProductSearchResults.SearchResults.map(({ Sku }) => Sku))
-  return {
-    stocks,
-    page: p,
-    headers,
-    nPages: json.ProductSearchResults.Pagination.TotalNumberOfPages,
-    json
-  }
-  */
 }
 
-// const getAllPages = async (lang) => {
 const getAllPages = async (cli) => {
-  // const lang = cli.flags.language
-  // const o = await getPage(lang)
   const o = await getPage(cli)
-  // const { nPages } = o
   const nPages = o.ProductSearchResults.Pagination.TotalNumberOfPages
   let r
   const gp = []
   for (r = 1; r < nPages; ++r) {
-    // gp.push(getPage(lang, r + 1))
     gp.push(getPage(cli, r + 1))
   }
   const z = await Promise.all(gp)
   z.push(o)
-  return z.reduce((a, json) => [...a, ...json.ProductSearchResults.SearchResults], [])
-}
+  const products = z.reduce((a, json) => [...a, ...json.ProductSearchResults.SearchResults], [])
 
-/*
-const available = (pages) => pages.reduce((a, { stocks, json }) => {
-  const them = json.ProductSearchResults.SearchResults.filter((x) => stocks.indexOf(x.Sku) !== -1)
-  return [...a, ...them]
-}, [])
-*/
+  if (!cli || !cli.flags) {
+    return products
+  }
+
+  if (cli.flags.inStock) {
+    return products.filter(({ IsOutOfStock }) => !IsOutOfStock)
+  }
+
+  if (cli.flags.inStock === false) {
+    return products.filter(({ IsOutOfStock }) => IsOutOfStock)
+  }
+
+  return products
+}
 
 const parseStore = (s) => {
   const rawAddress = s.querySelector('address').textContent
@@ -82,7 +77,7 @@ const parseStore = (s) => {
 
 const products = async (cli) => {
   const products = await getAllPages(cli)
-  const productsFixed = json.map(({ Url, ...product }) => ({
+  const productsFixed = products.map(({ Url, ...product }) => ({
     ...product,
     Url: `https://www.sqdc.ca${Url}`
   }))
@@ -112,14 +107,16 @@ const supportedLocations = [
     support: 'full'
   },
   {
-    id: 'other',
+    id: 'other'
   }
 ]
 
+/*
 const isSupportedLocation = (l) => {
   l = l.toLowerCase()
   return !supportedLocations.find(({ id, aliases }) => [id, ...aliases].map((s) => s.toLowerCase()).indexOf(l) === -1)
 }
+*/
 
 const locations = () => supportedLocations.filter(({ support }) => support)
 locations.description = 'List supported countries and provinces/states'
